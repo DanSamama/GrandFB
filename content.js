ae627963b79a9f92b99c9099ea2ddd59c116f599
Dan Samama <dan.samama@gmail.com>
2016-08-25 15:30:36 +0300

    Removing non usable CSS

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 var element = document.querySelector('#leftCol');
 element.parentElement.removeChild(element);
 
-var fucker = document.querySelector('#mainContainer');
-element.parentElement.removeChild(element);
+alert("Hello");
+
+
+
