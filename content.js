ff15dfa12edffc67784e7b12ef6337b7b4dc245f
Dan Samama <dan.samama@gmail.com>
2016-08-25 21:02:24 +0300

    Improving the UX

--- content.js
+++ content.js
@@ -1,7 +1,31 @@
+
+// Get rid of the left column
+$(document).ready(function () {
+
+
+ $("img").each(function(){
+        var currentImg = $(this);
+        var btn = $("<button>").text("+").click(function(e){
+            e.stopPropagation();
+            $(this).prev("img").width($(this).prev("img").width() + 100);
+        });
+
+        currentImg.parent().append(btn)
+    });
+    var element = document.querySelector('#leftCol');
+element.parentElement.removeChild(element);
+
+});
+
+
 var element = document.querySelector('#leftCol');
 element.parentElement.removeChild(element);
 
-alert("Hello");
 
+// Setting the newsfeed to stretch on all the page
+var globalContainer = $("#globalContainer").width("100%");
+var content = $("#content").width("100%");
+var mainContainer = $("#mainContainer").width("100%");
+var contentArea = $("#contentArea").width("100%");
 
 
