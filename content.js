b6e77093c373c2ae0d921c82085269a26f8b94d9
Dan Samama <dan.samama@gmail.com>
2016-08-25 11:43:32 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,5 @@
+var element = document.querySelector('#leftCol');
+element.parentElement.removeChild(element);
+
+var fucker = document.querySelector('#mainContainer');
+element.parentElement.removeChild(element);
